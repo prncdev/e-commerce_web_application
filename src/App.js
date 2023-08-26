2b6ef3883cdbc017833b54c9141d9f50e217bcf1
Prince Chaudhary <princedavi94@gmail.com>
2023-08-26 20:35:48 +0530

    In react data is our UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      catagories: [
+        { id: 1, title: 'Hats' },
+        { id: 2, title: 'Jackets' },
+        { id: 3, title: 'Sneakers' },
+        { id: 4, title: 'Womens' },
+        { id: 5, title: 'Mens' },
+      ]
+    }
+  }
+  render() {
+    return (
+      <main className='catagories-container'>
+        {this.state.catagories.map(({id, title}) => (
+          <section key={id} className="catagory-container">
+            {/* <img src="" alt="" /> */}
+            <section className="catagory-body-container">
+              <h2 className='product-title'>{title}</h2>
+              <p>Shop Now</p>
+            </section>
+          </section>
+        ))}
+
+      </main>
+    )
+  };
+};
 
 export default App;
