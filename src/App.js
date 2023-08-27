2572da0d641b841f575ae5aae57a94604ae6b33d
Prince Chaudhary <princedavi94@gmail.com>
2023-08-27 08:58:03 +0530

    Removed one object from array structure

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
         { id: 2, title: 'Jackets' },
         { id: 3, title: 'Sneakers' },
         { id: 4, title: 'Womens' },
-        { id: 5, title: 'Mens' },
       ]
     }
   }
