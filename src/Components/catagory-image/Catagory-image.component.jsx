acbe1f6738427564f66968d233cf43733be163e2
Prince Chaudhary <princedavi94@gmail.com>
2023-09-07 09:55:24 +0530

    Deleted some remaing commnents

--- src/Components/catagory-image/Catagory-image.component.jsx
+++ src/Components/catagory-image/Catagory-image.component.jsx
@@ -1,8 +1,6 @@
 import './catagory-image.styles.scss';
 
 function CatagoryImage({ image }) {
-  // eslint-disable-next-line no-lone-blocks
-  {/* In order to have inline css on a JSX element, we can do that with `style` props and double curly brackets. */}
   return <div className='background-image' style={{ backgroundImage: `url(${image})` }} />
 }
 
